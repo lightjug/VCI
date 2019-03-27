9c04ed4b5d17bad28ba9753ba1ee3fea0b7db4a8
lightjug <lightkyoro+github@gmail.com>
2019-03-27 15:21:31 +0900

    余分なコード削除
    余分なコード削除

--- lua/たらい.lua
+++ lua/たらい.lua
@@ -4,30 +4,6 @@
 print("init")
 vci.state.Set("isPlaySound",false)
 
-function update()
-end
-
-function updateAll()
-
-end
-
----[SubItemの所有権]アイテムをグラッブしてグリップボタンを押すと呼ばれる。
----@param use string @押されたアイテムのSubItem名
-function onUse(use)
-end
-
----[SubItemの所有権]アイテムにCollider(Trigger)が接触したときに呼ばれる。
----@param item string @SubItem名
----@param hit string @Collider名
-function onTriggerEnter(item, hit)
-end
-
----[SubItemの所有権]アイテムにCollider(Trigger)が離れたときに呼ばれる。
----@param item string @SubItem名
----@param hit string @Collider名
-function onTriggerExit(item, hit)
-end
-
 ---[SubItemの所有権]アイテムにCollider(not Trigger)が接触したときに呼ばれる。
 ---@param item string @SubItem名
 ---@param hit string @Collider名
@@ -38,25 +14,6 @@ function onCollisionEnter(item, hit)
       vci.assets._ALL_PlayAudioFromIndex(0)
       vci.state.Set("isPlaySound",true)
     end
-end
-
----[SubItemの所有権]アイテムにCollider(not Trigger)が離れたときに呼ばれる。
----@param item string @SubItem名
----@param hit string @Collider名
-function onCollisionExit(item, hit)
-end
-
----[SubItemの所有権]アイテムをGrabしたときに呼ばれる。
----@param target string @GrabされたSubItem名
-
-function onGrab(target)
-
-end
-
----[SubItemの所有権]アイテムをUngrabしたときに呼ばれる。
----@param target string @UngrabされたSubItem名
-function onUngrab(target)
-
 end
 
 
